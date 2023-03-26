9d18bcb4a0a270574c1c178668e3c95bbf1cd874
yangyulu <yangyulu1114@163.com>
2023-03-26 16:12:21 -0700

    Creating HTTP controllers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import HelloController
+    from "./controllers/hello-controller.js"
+
 const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+HelloController(app)
 app.listen(4000)
