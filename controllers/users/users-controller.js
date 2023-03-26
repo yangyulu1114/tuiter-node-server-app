fd500ebe6d63922d8a53ae9bfad4e2c9a2bb1f4c
yangyulu <yangyulu1114@163.com>
2023-03-26 16:41:39 -0700

    Sending query parameters to a Web server

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -5,6 +5,13 @@ const UserController = (app) => {
 }
 
 const findUsers = (req, res) => {
+    const type = req.query.type
+    if(type) {
+        const usersOfType = users
+            .filter(u => u.type === type)
+        res.json(usersOfType)
+        return
+    }
     res.json(users)
 }
 
