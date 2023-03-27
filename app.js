b36fc9e769ed09af25a8815d3be677dbd518f512
yangyulu <yangyulu1114@163.com>
2023-03-26 18:25:32 -0700

    Implementing Tuiter RESTful Web service APIs

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import HelloController
     from "./controllers/hello-controller.js"
 import UserController
     from "./controllers/users/users-controller.js"
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
 
 const app = express()
 app.use(express.json());
+TuitsController(app);
 HelloController(app)
 UserController(app)
 app.listen(4000)
---
+++ controllers/tuits/tuits-controller.js
@@ -0,0 +1,15 @@
+import posts from "./tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {}
+const findTuits  = (req, res) => {}
+const updateTuit = (req, res) => {}
+const deleteTuit = (req, res) => {}
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
+
