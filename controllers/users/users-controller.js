b50d8b9175c2d0a098eedd63ce372b1535c0ae2c
yangyulu <yangyulu1114@163.com>
2023-03-26 17:29:47 -0700

    Updating data in a Web server with Postman

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -5,6 +5,7 @@ const UserController = (app) => {
     app.get('/api/users/:uid', findUserById);
     app.post('/api/users', createUser);
     app.delete('/api/users/:uid', deleteUser);
+    app.put('/api/users/:uid', updateUser);
 }
 
 const findUsers = (req, res) => {
@@ -36,6 +37,17 @@ const deleteUser = (req, res) => {
     const userId = req.params['uid'];
     users = users.filter(usr =>
         usr._id !== userId);
+    res.sendStatus(200);
+}
+
+const updateUser = (req, res) => {
+    const userId = req.params['uid'];
+    const updates = req.body;
+    users = users.map((usr) =>
+        usr._id === userId ?
+            {...usr, ...updates} :
+            usr
+    );
     res.sendStatus(200);
 }
 
