569ca40f5b15e4f61ef7bd8653b4765cfb3fdb75
yangyulu <yangyulu1114@163.com>
2023-04-08 22:13:07 -0700

    configure connection string

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+
 const app = express()
 app.use(cors())
 app.use(express.json());
